5d84041391cc80f6eba8faedf07536c622c85199
AssafZohar <zoharassaf@mail.tau.ac.il>
2021-09-11 12:22:25 +0300

    calculations file

--- code/calculations.py
+++ code/calculations.py
@@ -63,6 +63,7 @@ def plot_disc(number_of_nodes_regular):
     plt.savefig(r"test\discilinated_plot_{}.png".format(number_of_nodes_regular))       
 
 def calculate_dynamics(t_max):
+    # if you don't use a lattice already calculated use the get_disclinated_lattice function to get a lattice ready for use
     c = (10 + (10-1) * 5) * 5
     lattice = DiscilinatedLattice(c)
     lattice.locations = numpy.loadtxt("flat_locations.csv", delimiter=',')
@@ -74,26 +75,6 @@ def calculate_dynamics(t_max):
         a += lattice.mode_spatial_by_frequency(value + 10**-13, w, used_values)
         numpy.append(used_values, value)
 
-    print(len(a))
-    
-    for val in a:
-        if val[0] < 0:
-            print(val[0])
-            val[0] = 0
-        ax = plt.subplot()
-        X = lattice.locations[:,0]
-        Y = lattice.locations[:,1]
-        cmhot = plt.get_cmap("coolwarm")
-        im = ax.scatter(X, Y, c=val[1], cmap=cmhot, s=100)
-        plt.colorbar(im, ax=ax)
-        plt.title("Mode spatial distribution for $\omega$={0:9.2f}".format(numpy.sqrt(val[0])), fontsize=18)
-        plt.xlabel("X (arbitrary)", fontsize=17)
-        plt.ylabel("Y (arbitrary)", fontsize=17)
-        filename = r"..\plots\modes\discilinated_locations2w={0:9.5f}{1:0.3f}.png".format(numpy.sqrt(val[0]), numpy.random.sample())
-        plt.savefig(filename)
-        plt.close()
-    
-    return
     X = lattice.locations[:,0]
     Y = lattice.locations[:,1]
     test_vector = numpy.zeros((275,))
@@ -156,30 +137,62 @@ def gaussian(x,y):
     return numpy.exp(-((x - x_mean) **2 + (y - y_mean)**2)/4)
     
 
-def main():
-    c = (10 + (10-1) * 5) * 5
-    c2 = (5+10+15+20+25 +30 +35)
-    c3 = 1 + (12 + (10-1)*6)*5
-    """
-    lattice = TriangularCenteredLattice(c3)
-    #lattice.locations = numpy.loadtxt("flat_locations.csv", delimiter=',')
+def get_triangular_lattice(num_of_nodes):
+    lattice = TriangularCenteredLattice(num_of_nodes)
     lattice.set_locations()
     a = numpy.ndarray.flatten(lattice.locations)
     result = minimize(lattice.energy_for_optimization, a)
-    print(result.success)
+    if not result.success:
+        raise("could not minimize enery")
     lattice.set_dynamic_matrix()
-    w, vec = lattice.find_eigen_values()
-    print(w)
-    w[0] = 0
-    w_for_plot = numpy.sqrt(w)
-    """
-    #print(numpy.median(w_for_plot))
+    return lattice
+
+
+def get_disclinated_lattice(num_of_nodes):
+    lattice = DiscilinatedLattice(num_of_nodes)
+    lattice.set_locations()
+    a = numpy.ndarray.flatten(lattice.locations)
+    result = minimize(lattice.energy_for_optimization, a)
+    if not result.success:
+        raise("could not minimize enery")
+    lattice.set_dynamic_matrix()
+    return lattice   
+
+def plot_spatial():
+    # if you don't use a lattice already calculated use the get_disclinated_lattice function to get a lattice ready for use
+    c = (10 + (10-1) * 5) * 5
     lattice = DiscilinatedLattice(c)
     lattice.locations = numpy.loadtxt("flat_locations.csv", delimiter=',')
-    lattice.set_dynamic_matrix()
+    lattice.set_dynamic_matrix([270, 269, 270 - 50])
+    w, vec = lattice.find_eigen_values()
+    used_values = numpy.array([-1])
+    a = []
+    for value in w:
+        a += lattice.mode_spatial_by_frequency(value + 10**-13, w, used_values)
+        numpy.append(used_values, value)
+
+    print(len(a))
+    
+    for val in a:
+        if val[0] < 0:
+            print(val[0])
+            val[0] = 0
+        ax = plt.subplot()
+        X = lattice.locations[:,0]
+        Y = lattice.locations[:,1]
+        cmhot = plt.get_cmap("coolwarm")
+        im = ax.scatter(X, Y, c=val[1], cmap=cmhot, s=100)
+        plt.colorbar(im, ax=ax)
+        plt.title("Mode spatial distribution for $\omega$={0:9.2f}".format(numpy.sqrt(val[0])), fontsize=18)
+        plt.xlabel("X (arbitrary)", fontsize=17)
+        plt.ylabel("Y (arbitrary)", fontsize=17)
+        filename = r"..\plots\modes\discilinated_locations2w={0:9.5f}{1:0.3f}.png".format(numpy.sqrt(val[0]), numpy.random.sample())
+        plt.savefig(filename)
+        plt.close()
+
+def plot_dos(lattice):
     w, vec = lattice.find_eigen_values()
     w_for_plot = numpy.sqrt(w)
-    
     
     ax = sns.distplot(w_for_plot, kde=True, rug=True, hist=False, vertical=True, kde_kws={"gridsize": 500, "cut":5, "bw": 0.2})
     ax.set_xticklabels(ax.get_xticks(), size = 12)
@@ -189,117 +202,6 @@ def main():
     plt.ylabel(r"$\omega$ $\left[\left(\frac{\varepsilon}{m\sigma ^ 2} \right) ^ \frac{1}{2} \right]$", fontsize=20)
     plt.xlabel('Density', fontsize=20)
     plt.show()
-    
-    
-
 
 if __name__ == "__main__":
-    main()
-    #plot_multiple_lattices()
-    #calculate_dynamics(20)
-
-    
-
-    """
-    c = (10 + (10-1) * 5) * 5
-    c2 = (5+10+15+20+25 +30 +35)
-    c3 = 1 + (12 + (10-1)*6)*5
-    
-    lattice = DiscilinatedLattice(c)
-    lattice.locations = numpy.loadtxt("flat_locations.csv", delimiter=',')
-    #a = numpy.ndarray.flatten(lattice.locations)
-    #result = minimize(lattice.energy_for_optimization, a)
-    #print(result.success)
-    lattice.set_dynamic_matrix()
-    w, vec = lattice.find_eigen_values()
-    used_values = numpy.array([-1])
-    a = []
-    for value in w:
-        a += lattice.find_vectors_and_repricosity(value + 10**-13, w, used_values)
-        numpy.append(used_values, value)
-
-    x= []
-    y =[]
-    used_values = numpy.array([-1])
-    for b in a:
-        if (b[2] < 20):
-            c = lattice.find_vectors_and_repricosity2(b[0], w, used_values)
-            vec = c[0][1]
-            print(len(c))
-            fig, ax = plt.subplots()
-            cmhot = plt.get_cmap("coolwarm")
-            im = ax.scatter(lattice.locations[:,0], lattice.locations[:,1], c=vec, cmap=cmhot, vmin=-1, vmax=1)
-            ax.set_label("IPR={0:9.3f}".format(b[2]))
-            plt.colorbar(im, ax=ax)
-            plt.title("Mode spatial distribution for $\omega$={0:9.2f}".format(numpy.sqrt(b[0])), fontsize=18)
-            plt.xlabel("X (arbitrary)", fontsize=17)
-            plt.ylabel("Y (arbitrary)", fontsize=17)
-            plt.savefig(r"..\plots\IPR\fdiscilinated_IPR_w={0:9.5f}.png".format(b[0]))  
-    """     
-
-    """
-    print(len(x))
-    fig, ax = plt.subplots()
-    ax.plot(x,y, '.', markersize=8)
-    line = mlines.Line2D([-1, 50], [15, 15], color='red')
-    line.set_label("IPR = 15")
-    ax.add_line(line)
-    ax.set_xlim(-0.8, 50)
-    plt.xlabel("$\omega$ (arbitrary)", fontsize=17)
-    plt.ylabel("IPR", fontsize=17)
-    plt.title("Discilinated lattice IPR by frequency", fontsize=18)
-    ax.legend()
-    plt.show()
-    """
-    
-        
-    #print(lattice.locations)
-    #w, vec = lattice.find_eigen_values()
-    #plot_density_of_states(w)
-    """
-    for i in range(1,10):
-        print(i)
-        lattice = DiscilinatedLattice(c, i)
-        lattice.set_locations()
-
-        a = numpy.ndarray.flatten(lattice.locations)
-        result = minimize(lattice.energy_for_optimization, a)
-        if not result.success:
-            continue
-
-        nodes = numpy.ndarray.reshape(result.x, (-1,2))
-
-        lattice.locations = nodes # numpy.loadtxt("flat_locations.csv", delimiter=',')
-        plt.figure(clear=True)
-        plt.plot(nodes[:,0], nodes[:,1], '.')
-        plt.title("triangular lattice locations for lattice const a={} ".format(lattice.lattice_constant))
-        plt.savefig(r"..\plots\lattice_const\discilinated_locations_plot_{}.png".format(lattice.lattice_constant))  
-        lattice.set_dynamic_matrix()
-        w, vec = lattice.find_eigen_values()
-        plt.figure(clear=True)
-        plt.plot(w, '.')
-        plt.title("triangular lattice frequencies for lattice const a={} ".format(lattice.lattice_constant))
-        plt.xlabel("Frequency number")
-        plt.ylabel(r"$\omega$")
-        plt.savefig(r"..\plots\lattice_const\discilinated_plot_{}.png".format(lattice.lattice_constant))  
-    used_values = numpy.array([-1])
-    a = []
-    for value in w:
-        a += lattice.find_vectors_and_repricosity(value + 10**-13, w, used_values)
-        numpy.append(used_values, value)
-
-    numpy.savetxt("IPR.csv" ,numpy.array(a), delimiter=",")
-
-    
-    X = x_vector
-    Y = y_vector
-    Z = vec2[:,0]
-    print(Z)
-    # plt.contourf(X,Y,Z) plt.pcolormesh(X,Y,Z, shading="auto")
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    surf = ax.scatter3D(X, Y, Z)
-    plt.title("Mode spatial distribution for w={}".format(w2[0]))
-    
-    plt.show()
-
-    #print(lattice.system_energy())"""
+    print("You should put some calculation here")
